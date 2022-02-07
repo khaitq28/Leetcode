8f8ec24ff8c34e79fc0ed3898f796e5be5e3f142
quangkhai88 <canhcung88@gmail.com>
2022-02-07 22:46:46 +0100

    update

--- src/recursive/BinarySearch.java
+++ src/recursive/BinarySearch.java
@@ -6,9 +6,9 @@ public class BinarySearch {
 
         // { 1,2,3,4,5,6,7}
 
-        int[] arr = {1,3,4,5,6,7};
+        int[] arr = {1,3,4,5,6,7,10,45,89};
 
-        System.out.println(binarySearch(arr,0,arr.length-1, 2));
+        System.out.println(binarySearch(arr,0,arr.length-1, 10));
 
     }
 
