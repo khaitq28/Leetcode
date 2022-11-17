6a62e096478a46d04c2f3765482d90d4e2ae4f16
quangkhai88 <canhcung88@gmail.com>
2022-11-17 23:33:11 +0100

    update

--- src/backtrack/GenerateParathese.java
+++ src/backtrack/GenerateParathese.java
@@ -12,61 +12,46 @@ public class GenerateParathese {
 
 
     private final Set<String> all = new HashSet<>();
-    private List<String> param;
 
-    public void generateCombination() {
-        init(0);
-        System.out.println("all");
-        System.out.println(all);
+    public static void main(String[] args) {
+
+        GenerateParathese generateParathese = new GenerateParathese();
+        generateParathese.generate(new StringBuilder());
+        System.out.println(generateParathese.all);
     }
 
-    private void init(int i) {
-        boolean[] marked = new boolean[this.param.size()];
-        marked[i] = true;
-        List<String> arr = new ArrayList<>();
-        String current = this.param.get(i);
-        arr.add(current);
-        backtrack(marked, arr);
-    }
+    int c1; //number of (
+    int c2; // number of )
+    int n = 9; //number of pair
 
-    private void backtrack(boolean[] marked, List<String> arr) {
-        if(arr.size() == param.size() && arr.get(arr.size()-1).equals(")")) {
-            System.out.println(arr);
-            all.add(arr.stream().reduce("", (s,v) -> s+v));
+
+    /*
+        logic: at position i, it could be '(' if number c1 < n, or it could be ')' if c2  < c1
+     */
+    private void generate(StringBuilder sb) {
+
+        if (sb.length() == n * 2) {
+            all.add(sb.toString());
             return;
         }
-        for (int i = 0; i < param.size(); i++) {
-            if(!marked[i]) {
-                String tem = param.get(i);
-                marked[i] = true;
-                arr.add(tem);
-                backtrack(marked,arr);
-                arr.remove(arr.size()-1);
-                marked[i] = false;
-            }
+        if (c1 < n) {
+            sb.append("(");
+            c1++;
+            generate(sb);
+            c1--;
+            sb.deleteCharAt(sb.length()-1);
         }
+
+        if (c2 < c1) {
+            sb.append(")");
+            c2++;
+            generate(sb);
+            c2--;
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+
     }
-
-    public static void main(String ...art) {
-        List<String> param = Arrays.asList("(",")","(",")","(",")");
-        GenerateParathese main = new GenerateParathese();
-        main.param = param;
-
-        /*
-        main.generateCombination();
-        */
-    }
-
-    public void generate2() {
-        int n = 3;
-
-        boolean[] marked = new boolean[n];
-        marked[0] = true;
-        List<String> arr = new ArrayList<>();
-        arr.add("()");
-//        backtrack2(arr, marked);
-    }
-
 
 
 }
